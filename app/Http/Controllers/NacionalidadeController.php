f2ec341d7f3ec6a8a5813ef650dae745cb5faa96
cjuniordev <carlosrogeriocr02@gmail.com>
2022-02-24 07:58:52 -0300

    feat(backend): CRUD nacionalidades

--- app/Http/Controllers/NacionalidadeController.php
+++ app/Http/Controllers/NacionalidadeController.php
@@ -3,36 +3,51 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Models\Nacionalidade;
 
 class NacionalidadeController extends Controller
 {
+    private $nacionalidades;
 
+    public function __construct(Nacionalidade $nacionalidades){
+        $this->nacionalidades = $nacionalidades;
+    }
 
     public function index()
     {
+        $nacionalidades = $this->nacionalidades->all();
+        return view('nacionalidade.index', compact('nacionalidades'));
+    }
+
+    public function create(){
+        return view('nacionalidade.crud');
     }
 
 
-    public function create()
-    {
+    public function store(Request $request){
+        $nacionalidade = new Nacionalidade();
+        $nacionalidade->nacionalidade = $request->input('nacionalidade');
+        $nacionalidade->save();
+
+        return redirect('nacionalidade');
     }
 
+    public function edit($id){
+        $nacionalidade = $this->nacionalidades->find($id);
 
-    public function store(Request $request)
-    {
+        return view('nacionalidade.crud', compact('nacionalidade'));
     }
 
-    public function edit($id)
-    {
+    public function update(Request $request, $id){
+        $this->nacionalidades->find($id)->update(['nacionalidade' => $request->input('nacionalidade')]);
+
+        return redirect('nacionalidade');
     }
 
-    public function update(Request $request, $id)
-    {
-    }
+    public function destroy($id){
+        $nacionalidade = $this->nacionalidades->find($id);
 
-    public function destroy($id)
-    {
+        $nacionalidade->delete();
+        return redirect(route('nacionalidade.index'));
     }
 }
