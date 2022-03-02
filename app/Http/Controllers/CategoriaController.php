cba4f36adf5bc26f34dd1613e236de0793c4840a
Marcos Mello <Marcosvinicius-vm@outlook.com>
2022-03-02 18:45:12 -0300

    Feat: Criei os controllers de categoria e produto.

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,38 +2,135 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\Http\Requests\CategoriaRequest;
 use App\Models\Categoria;
+use App\Http\Requests\CategoriaStoreRequest;
+use App\Http\Requests\CategoriaUpdateRequest;
 
 class CategoriaController extends Controller
 {
+    private $categorias;
 
-
-
-    public function index()
+    public function __construct(Categoria $categoria) //Inicia o objeto com o seu modelo
     {
+        $this->categorias = $categoria;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(CategoriaRequest $request)
+    {
+        $categorias = $this->categorias->all();
+        return view('categoria.index', compact('categorias'));
     }
 
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
+        return view('categoria.crud');
     }
 
-
-    public function store(Request $request)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\CategoriaStoreRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CategoriaStoreRequest $request)
     {
+        $datas = $request->all();
+        $categoria = $this->categorias->create($datas);
+
+        $log = [
+            'id' => $categoria->id,
+            'name' => $categoria->categoria,
+            'text' => 'cadastrou uma nova categoria',
+            'category' => 'Categorias'
+        ];
+
+        $this->doLogs($log); //função de controller.php que cria os logs
+
+        return redirect(route('categoria.index'))->with('success', 'Categoria cadastrada com sucesso!');
     }
 
-    public function edit($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CategoriaRequest $request, $id)
     {
+        $categoria = $this->categoria->find($id);
+
+        return json_encode($categoria);
     }
 
-    public function update(Request $request, $id)
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CategoriaRequest $request, $id)
     {
+        $categoria = $this->categorias->find($id);
+        return view('categoria.crud', compact('categoria'));
     }
 
-    public function destroy($id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\CategoriaUpdateRequest  $request
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CategoriaUpdateRequest $request, $id)
     {
+        $datas = $request->all();
+        $categoria = $this->categorias->find($id);
+
+        $categoria->update($datas);
+
+        $log = [
+            'id' => $categoria->id,
+            'name' => $categoria->categoria,
+            'text' => 'alterou a categoria',
+            'category' => 'Categorias'
+        ];
+
+        $this->doLogs($log); //função de controller.php que cria os logs
+
+        return redirect(route('categoria.index'))->with('success', 'Categoria alterada com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categoria  $categoria
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CategoriaRequest $request, $id)
+    {
+        $categoria = $this->categorias->find($id);
+
+        $categoria->delete();
+
+        $log = [
+            'id' => $categoria->id,
+            'name' => $categoria->categoria,
+            'text' => 'excluiu a categoria',
+            'category' => 'Categorias'
+        ];
+
+        $this->doLogs($log); //função de controller.php que cria os logs
+
+        return redirect(route('categoria.index'))->with('success', 'Categoria excluída com sucesso!');
     }
 }
--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,45 +2,149 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Categoria;
 use App\Models\Produto;
-use Illuminate\Http\Request;
+use App\Http\Requests\ProdutoStoreRequest;
+use App\Http\Requests\ProdutoRequest;
+use App\Http\Requests\ProdutoUpdateRequest;
+use App\Models\Categoria;
 use Illuminate\Support\Facades\Storage;
-
 
 class ProdutoController extends Controller
 {
+    private $produtos;
 
-    public function index()
+    public function __construct(Produto $produto) //Inicia o objeto com o seu modelo
     {
+        $this->produtos = $produto;
     }
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(ProdutoRequest $request)
+    {
+        $produtos = $this->produtos->all();
+        return view('produto.index', compact('produtos'));
+    }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
+        $categorias = Categoria::all();
+        return view('produto.crud', compact('categorias'));
     }
 
-
-    public function store(Request $request)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\ProdutoStoreRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProdutoStoreRequest $request)
     {
+        $datas = $request->all();
+        $datas['imagem'] = $request->file('imagem')->store('', 'public');
+
+        $produto = $this->produtos->create($datas);
+
+        $log = [
+            'id' => $produto->id,
+            'name' => $produto->nome,
+            'text' => 'cadastrou um novo produto',
+            'category' => 'Produtos'
+        ];
+
+        $this->doLogs($log); //função de controller.php que cria os logs
+
+        return redirect(route('produto.index'))->with('success', 'Produto cadastrado com sucesso!');
     }
 
-
-    public function show($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProdutoRequest $request, $id)
     {
+        $produto = $this->produtos->find($id);
+        $produto['categoria'] = Categoria::find($produto->categoria_id)->categoria;
+        return json_encode($produto);
     }
 
-
-    public function edit($id)
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProdutoRequest $request, $id)
     {
+        $produto = $this->produtos->find($id);
+        $categorias = Categoria::all();
+        return view('produto.crud', compact('produto'), compact('categorias'));
     }
 
-
-    public function update(Request $request, $id)
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\ProdutoUpdateRequest  $request
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProdutoUpdateRequest $request, $id)
     {
+        $datas = $request->all();
+        $produto = $this->produtos->find($id);
+
+        if ($request->hasFile('imagem'))
+        {
+            Storage::delete('public/' . $produto->imagem);
+            $datas['imagem'] = $request->file('imagem')->store('', 'public');
+        }
+
+        $produto->update($datas);
+
+        $log = [
+            'id' => $produto->id,
+            'name' => $produto->nome,
+            'text' => 'alterou o produto',
+            'category' => 'Produtos'
+        ];
+
+        $this->doLogs($log); //função de controller.php que cria os logs
+
+        return redirect(route('produto.index'))->with('success', 'Produto alterado com sucesso!');
     }
 
-    public function destroy($id)
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Produto  $produto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProdutoRequest $request, $id)
     {
+        $produto = $this->produtos->find($id);
+
+        Storage::delete('public/' . $produto->imagem);
+        $produto->delete();
+
+        $log = [
+            'id' => $produto->id,
+            'name' => $produto->nome,
+            'text' => 'excluiu o produto',
+            'category' => 'Produtos'
+        ];
+
+        $this->doLogs($log); //função de controller.php que cria os logs
+
+        return redirect(route('produto.index'))->with('success', 'Produto excluído com sucesso!');
     }
 }
