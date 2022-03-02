ae154d920c9214b39b64b4ba57a7edf18041e5e5
Marcos Mello <Marcosvinicius-vm@outlook.com>
2022-03-02 18:45:42 -0300

    Feat: Adicionei a função de log em Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Log;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function doLogs($variable)
+    {
+        $logMessage = 'O usuário ' . '[' . auth()->id() . '] - ' . auth()->user()->name . ' ' . $variable['text'] . ' ' . '[' . $variable['id'] . '] - ' . $variable['name'];
+        Log::create(['log' => $logMessage, 'category' => $variable['category']]);
+    } //Cria os logs
 }
